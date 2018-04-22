3145ed3797906124b5351b3f4ba5389d54472198
Wojtek <nwojtek@gmail.com>
2018-04-22 14:16:33 +0200

    Lab1, migracja z glwnego repo

---
+++ lab1/src/zad2/zad2.java
@@ -0,0 +1,16 @@
+package zad2;
+
+public class zad2 {
+	/**
+	 * Lab1 - Zad2
+	 * @author Wojtek Niedzielski
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NrbVal nrb = new NrbVal("20113010170021100249900004");
+		System.out.println(nrb.getBi());
+		System.out.println(nrb.getVal());
+	}
+
+}
